2ba4c46fd3a36c2244c22451957f6afbba9a6598
Juhani Ovalo <juhani.ovalo@gmail.com>
2024-06-23 23:53:49 +0300

    Social media icons added.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,14 @@
 const Navbar = () => ( 
 <div className="navbar bg-base-100 shadow-md">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Juhani Ovalo</a>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>
-            Parent
-          </summary>
-          <ul className="p-2 bg-base-100 rounded-t-none">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
-          </ul>
-        </details>
-      </li>
+      <li><a href="https://www.google.com" className="fab fa-github text-2xl text-blue-500"/></li>
+      <li><a href="https://www.google.com" className="fab fa-linkedin text-2xl text-blue-500"/></li>
+      <li><a href="https://www.google.com" className="fa-brands fa-mastodon text-2xl text-blue-500"/></li>
+      <li><a href="https://www.google.com" className="fa-brands fa-bluesky text-2xl text-blue-500"/></li>
     </ul>
   </div>
 </div>
